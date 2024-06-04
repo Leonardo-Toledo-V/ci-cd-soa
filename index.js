0f5de437c7e75b8cdc0256a725b137e146f7b1c7
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-06-04 02:30:19 +0000

    update message2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req,res) => {
-	res.send('Hello World!update');
+	res.send('Hello World!update2');
 });
 
 app.listen(port,()=>{
